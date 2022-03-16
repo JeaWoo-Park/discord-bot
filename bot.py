86103d3fec950a5318acec5a3eea9f4ce1165295
JeaWoo-Park <55945935+JeaWoo-Park@users.noreply.github.com>
2022-03-17 00:29:02 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ from discord.ext import commands
 
 game = discord.Game("칼바람")
 bot = commands.Bot(command_prefix='!', status=discord.Status.online, activity=game)
-file = open("champion_list.txt",'r',encoding='euc-kr')
-cham = file.read().splitlines()
+file = open("champion_list.txt",'r',encoding='cp949')
+champ = file.read().splitlines()
 file.close()
 #cham = ['가렌','그라가스','갈리오']
 
