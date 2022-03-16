44e638d7bb35ece6cb5db20f0f11d52fc52d62e8
JeaWoo-Park <55945935+JeaWoo-Park@users.noreply.github.com>
2022-03-16 23:21:10 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,4 +84,4 @@ async def teamnow(ctx):
     await ctx.send(f"2팀 {team2}")
 
 access_token = os.environ["BOT_TOKEN"]
-bot.run('OTUzMzI2NDEyNTAzMzIyNzE0.YjC8aA.EjkQzd-eUrC4hrqWplEYexGUhes')  #봇의 토큰값
+bot.run('OTUzMzI2NDEyNTAzMzIyNzE0.YjC8aA.ghWGYzs5eC1-NWjTDbHAIoyRHY0')  #봇의 토큰값
